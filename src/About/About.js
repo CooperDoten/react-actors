22179c80edb788a0ecbf0fcd9dfd61e3a98102b2
CooperDoten <cooper.doten@gmail.com>
2020-09-15 20:03:33 -0500

    added all components, Header, About, Contact, Footer. Rendered to DOM]

---
+++ src/About/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+      return (
+        <div>
+            <h2>About</h2>
+            <p>
+                React Actors was founded in 2002 with 
+                the simple idea that building web apps 
+                should be intuitive, innovative and designed
+                around the user experience. 18 Years later
+                and React Actors is striving after that 
+                same ethos, to meet the needs of clients 
+                and companies all over the world.
+            </p>
+        </div>
+      );
+    }
+  }
+  
+  export default About;
---
+++ src/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+      return (
+        <div>
+            <h2>Contact</h2>
+            <p>(612)-928-0091</p>
+            <p>react.actors@gmail.com</p>
+        </div>
+      );
+    }
+  }
+  
+  export default Contact;
